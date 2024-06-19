2b43a6feee4aa219c6d587ff33f720f28984006a
leonardo munoz 🌑🌍 <168220131+leonardomunooz@users.noreply.github.com>
2024-06-19 19:44:29 +0000

    se ha agregado las pruebas a las funciones de conversion de divisas

---
+++ test.js
@@ -0,0 +1,40 @@
+// Importar la función sum del archivo app.js
+const { sum } = require('./app.js');
+
+// Comienza tu primera prueba
+test('adds 14 + 9 to equal 23', () => {
+    // Dentro de la prueba llamamos a nuestra función sum con 2 números
+    let total = sum(14, 9);
+
+    // Esperamos que la suma de esos 2 números sea 23
+    expect(total).toBe(23);
+});
+// one euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+
+test("One euro should be 1.07 dollars", () => {
+    // Importo la funcion desde app.js
+    const { fromEuroToDollar } = require('./app.js');
+
+    // Uso la función como debe ser usada
+    const dollars = fromEuroToDollar(3.5);
+
+    // Si 1 euro son 1.07 dólares, entonces 3.5 euros debe ser (3.5 * 1.07)
+    const expected = 3.5 * 1.07;
+
+    // Hago mi comparación (la prueba)
+    expect(dollars).toBe(expected); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+})
+test("One dollar should be 156,5 japan yen", () => {
+    const { fromDollarToYen } = require('./app.js');
+
+    const yenes = fromDollarToYen(5);
+
+    const expected = 5 * 156.5;
+    expect(yenes).toBe(expected)
+})
